34aecea0d404dfd4517bf27a45a946c3618034b5
Madina <madinadzanaeva670@gmail.com>
2026-02-09 16:44:07 +0300

    Инициализация + разметка щапки + базовая раскраска

---
+++ header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div class="header">
+        <a href="#">&#183; Главная</a>
+        <a href="#">&#183; О нас</a>
+        <a href="#">&#183; Новости / Объявления</a>
+        <a href="#">&#183; Faq / Помощь</a>
+        <a href="#">&#183; Контакты</a>
+        <a href="#">&#183; Вход / Регистрация</a>
+    </div>
+</body>
+</html>
