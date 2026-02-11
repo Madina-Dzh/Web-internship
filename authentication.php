990be76fa9bf8a2dea7b3a203a94801785ea1c3c
Madina <madinadzanaeva670@gmail.com>
2026-02-11 19:19:39 +0300

    Разметка формы аутентификации

--- authentication.php
+++ authentication.php
@@ -4,14 +4,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Вход в систему</title>
+    <link rel="stylesheet" href="./authentication.css">
 </head>
 <body class="site">
     <?php 
         include 'header.php';
     ?>
-
-    <div class="center">
-        
+    <div class="wrapper-auth">
+        <form id="auth"> 
+        <label for="ligin">Логин пользователя</label><br>
+        <input class="input-text" type="text" name="login" id="login" placeholder="Введите логин пользователя"/><br><br>
+        <label>Пароль пользователя</label><br>
+        <input class="input-text" type="password" name="password" id="password" placeholder="Введите пароль пользователя"/><br><br>
+        <input type="submit" value="Войти" name="btn-submit" id="btn-submit"/>
+        <a href="#">Регистрация</a>
+    </form>
     </div>
+    
 </body>
 </html>
