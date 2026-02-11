d2b8bfcfa42f84abe4953c2c98cd7d0acd443ceb
Madina <madinadzanaeva670@gmail.com>
2026-02-11 19:43:44 +0300

    Разметка для личного кабинета + прилипание шапки к верху страницы

---
+++ cabinet.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Личный кабинет</title>
+    <link rel="stylesheet" href="./cabinet.css">
+</head>
+<body class="site">
+    <?php 
+        include 'header.php';
+    ?>
+    <div class="cabinet">
+        <aside>
+
+        </aside>
+
+        <div class="wrapper-work">
+
+        </div>
+    </div>
+    
+</body>
+</html>
