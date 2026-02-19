5468c9e3a3f64f172da94a5e16ec63884259489e
Madina <madinadzanaeva670@gmail.com>
2026-02-19 10:27:53 +0300

    Добавлена визуализация ведомостей
    Соединение с бдЮ запрос, вывод в виде таблицы. Пока что только для 3 столбцов и без скролинга (на всю длину)

--- cabinet.php
+++ cabinet.php
@@ -1,3 +1,9 @@
+<?php 
+$mysql = new mysqli("localhost", "root", "", "internship");
+$mysql->query("SET NAMES 'utf8'");
+$query = "SELECT I.internship_code AS Номер, S.FIO AS Студент, S.Shifr_gr AS Группа, O.title AS Организация, U.code AS Код_предмета, T.FIO AS Преподаватель, P.start_date AS Дата_начала, P.end_date AS Дата_окончания, I.assessment AS Оценка, I.internship_status AS Статус, I.note AS Заметки, I.report AS Отчёт FROM Internship I INNER JOIN Practice P ON P.practice_code = I.practice_code INNER JOIN Student S ON I.student_code = S.Nom_stud LEFT JOIN Organization O ON I.organization_code = O.organization_code INNER JOIN subjects_in_cycle U ON U.id = P.subject_code LEFT JOIN Teacher T ON P.teacher = T.Tab_nom;";
+$res = $mysql->query($query);  
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -16,7 +22,12 @@
         </aside>
 
         <div class="wrapper-work">
-
+            <?php ; 
+                echo "<table><tr><th>Номер</th><th>Студент</th><th>Группа</th></tr>";
+                while ($row = mysqli_fetch_array($res)) {
+                    print("<tr><td>" . $row['Номер'] . "</td><td> " . $row['Студент'] ."</td><td> " . $row['Группа'] . "<br>");
+                }
+            ?>
         </div>
     </div>
     
