5c8c1cd5208175f83693b28a6240c312fac6582a
Madina <madinadzanaeva670@gmail.com>
2026-02-22 19:03:19 +0300

    Добавила блок раздела

--- cabinet.php
+++ cabinet.php
@@ -20,45 +20,49 @@ $res = $mysql->query($query);
     <aside></aside>
 
     <div class="wrapper-work">
-        <h2>Ведомости</h2>
 
-        <div class="work-panel-for-table">
-            <div class="wrapper-filtres">
-                <h3>Фильтры</h3>
-                <div class="filtr">
-                    <div class="left"><span>По имени студента</span></div>
-                    <div class="right"><input type="text"></div>
+        <!-- Раздел ведомостей -->
+        <div class="chapter">
+            <h2>Ведомости</h2>
+            <div class="work-panel-for-table">
+                <div class="wrapper-filtres">
+                    <h3>Фильтры</h3>
+                    <div class="filtr">
+                        <div class="left"><span>По имени студента</span></div>
+                        <div class="right"><input type="text"></div>
+                    </div>
+                    <div class="filtr">
+                        <div class="left"><span>По имени преподавателя</span></div>
+                        <div class="right"><input type="text"></div>
+                    </div>
+                    <div class="filtr">
+                        <div class="left"><span>По названию организации</span></div>
+                        <div class="right"><input type="text"></div>
+                    </div>
+                    <div class="filtr">
+                        <div class="left"><span>По коду практики</span></div>
+                        <div class="right"><input type="text"></div>
+                    </div>
                 </div>
-                <div class="filtr">
-                    <div class="left"><span>По имени преподавателя</span></div>
-                    <div class="right"><input type="text"></div>
-                </div>
-                <div class="filtr">
-                    <div class="left"><span>По названию организации</span></div>
-                    <div class="right"><input type="text"></div>
-                </div>
-                <div class="filtr">
-                    <div class="left"><span>По коду практики</span></div>
-                    <div class="right"><input type="text"></div>
+                
+                <div class="button">
+                    <button>Поиск</button>
+                    <button>Изменить</button>
+                    <button>Удалить</button>
                 </div>
             </div>
-            <div class="button">
-                <button>Поиск</button>
-                <button>Изменить</button>
-                <button>Удалить</button>
+            
+            <div class="wrapper-table">
+                <?php 
+                echo "<table><tr><th>№</th><th>Студент</th><th>Группа</th><th>Организация</th><th>Практика</th><th>Статус</th><th>Оценка</th><th>Преподаватель</th><th>Отчёт</th><th>Статус</th></tr>";
+                while ($row = mysqli_fetch_array($res)) {
+                    print("<tr><td>" . $row['Номер'] . "</td><td> " . $row['Студент'] ."</td><td> " . $row['Группа']  . "</td><td> " . $row['Организация']  . "</td><td> " . $row['Код_предмета']  . "</td><td> " . $row['Статус'] . "</td><td>". $row['Оценка'] . "</td><td>". $row['Преподаватель'] . "</td><td>". $row['Отчёт'] . "</td><td>". $row['Статус'] .  "</td></tr>");
+                }
+                echo "</table>";
+                ?>
             </div>
         </div>
-        
-        <div class="wrapper-table">
-            <?php 
-            echo "<table><tr><th>№</th><th>Студент</th><th>Группа</th><th>Организация</th><th>Практика</th><th>Статус</th><th>Оценка</th><th>Преподаватель</th><th>Отчёт</th><th>Статус</th></tr>";
-            while ($row = mysqli_fetch_array($res)) {
-                print("<tr><td>" . $row['Номер'] . "</td><td> " . $row['Студент'] ."</td><td> " . $row['Группа']  . "</td><td> " . $row['Организация']  . "</td><td> " . $row['Код_предмета']  . "</td><td> " . $row['Статус'] . "</td><td>". $row['Оценка'] . "</td><td>". $row['Преподаватель'] . "</td><td>". $row['Отчёт'] . "</td><td>". $row['Статус'] .  "</td></tr>");
-            }
-            echo "</table>";
-            ?>
-        </div>
-
+        <!-- Конец раздела ведомостей -->
         
     </div>
 </div>
