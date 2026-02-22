b53d499595be8adaaae974939b5d82ab7da42fef
Madina <madinadzanaeva670@gmail.com>
2026-02-22 18:52:52 +0300

    Исправлена структура вывода раздела (были незакрытые теги) + фиксируемая ширина контейнера

--- cabinet.php
+++ cabinet.php
@@ -17,66 +17,52 @@ $res = $mysql->query($query);
         include 'header.php';
     ?>
     <div class="cabinet">
-        <aside>
+    <aside></aside>
 
-        </aside>
+    <div class="wrapper-work">
+        <h2>Ведомости</h2>
 
-        <div class="wrapper-work">
-            <h2>Ведомости</h2>
-
-            <div class="work-panel-for-table">
-                <div class="wrapper-filtres">
-                    <h3>Фильтры</h3>
-                    <div class="filtr">
-                        <div class="left">
-                            <span>По имени студента</span>
-                        </div>
-                        <div class="right">
-                            <input type="text">
-                        </div>
-                    </div>
-                    <div class="filtr">
-                        <div class="left">
-                            <span>По имени преподавателя</span>
-                        </div>
-                        <div class="right">
-                            <input type="text">
-                        </div>
-                    </div>
-                    <div class="filtr">
-                        <div class="left">
-                            <span>По названию организации</span>
-                        </div>
-                        <div class="right">
-                            <input type="text">
-                        </div>
-                    </div>
-                    <div class="filtr">
-                        <div class="left">
-                            <span>По коду практики</span>
-                        </div>
-                        <div class="right">
-                            <input type="text">
-                        </div>
-                    </div>
+        <div class="work-panel-for-table">
+            <div class="wrapper-filtres">
+                <h3>Фильтры</h3>
+                <div class="filtr">
+                    <div class="left"><span>По имени студента</span></div>
+                    <div class="right"><input type="text"></div>
                 </div>
-                <div class="button">
-                    <button>Поиск</button>
-                    <Button>Изменить</Button>
-                    <button>Удалить</button>
+                <div class="filtr">
+                    <div class="left"><span>По имени преподавателя</span></div>
+                    <div class="right"><input type="text"></div>
+                </div>
+                <div class="filtr">
+                    <div class="left"><span>По названию организации</span></div>
+                    <div class="right"><input type="text"></div>
+                </div>
+                <div class="filtr">
+                    <div class="left"><span>По коду практики</span></div>
+                    <div class="right"><input type="text"></div>
                 </div>
             </div>
-            
-            <div class="wrapper-table">
-                <?php 
-                echo "<table><tr><th>№</th><th>Студент</th><th>Группа</th><th>Организация</th><th>Практика</th><th>Статус</th><th>Оценка</th><th>Преподаватель</th><th>Отчёт</th><th>Статус</th></tr>";
-                while ($row = mysqli_fetch_array($res)) {
-                    print("<tr><td>" . $row['Номер'] . "</td><td> " . $row['Студент'] ."</td><td> " . $row['Группа']  . "</td><td> " . $row['Организация']  . "</td><td> " . $row['Код_предмета']  . "</td><td> " . $row['Статус'] . "</th><td>". $row['Оценка'] . "</th><td>". $row['Преподаватель'] . "</th><td>". $row['Отчёт'] . "</th><td>". $row['Статус'] .  "<br>");
-                }
-                ?>
+            <div class="button">
+                <button>Поиск</button>
+                <button>Изменить</button>
+                <button>Удалить</button>
             </div>
         </div>
+        
+        <div class="wrapper-table">
+            <?php 
+            echo "<table><tr><th>№</th><th>Студент</th><th>Группа</th><th>Организация</th><th>Практика</th><th>Статус</th><th>Оценка</th><th>Преподаватель</th><th>Отчёт</th><th>Статус</th></tr>";
+            while ($row = mysqli_fetch_array($res)) {
+                print("<tr><td>" . $row['Номер'] . "</td><td> " . $row['Студент'] ."</td><td> " . $row['Группа']  . "</td><td> " . $row['Организация']  . "</td><td> " . $row['Код_предмета']  . "</td><td> " . $row['Статус'] . "</td><td>". $row['Оценка'] . "</td><td>". $row['Преподаватель'] . "</td><td>". $row['Отчёт'] . "</td><td>". $row['Статус'] .  "</td></tr>");
+            }
+            echo "</table>";
+            ?>
+        </div>
+
+        
     </div>
+</div>
+
     
 </body>
 </html>
